dfbd679765384ec33e00bb22b136e0e11c4c4262
klevismiho <klevisi@gmail.com>
2015-10-27 09:40:31 +0100

    Added gulp command as a default

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,14 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	rimraf   = require('rimraf'),
-	sequence = require('run-sequence');
+	sass = require('gulp-sass');
 
 
 // Cleans the build directory
-gulp.task('clean', function(cb) {
-	rimraf('./build', cb);
-});
-
-gulp.task('copy-motion-ui', function() {
-  return gulp.src('bower_components/motion-ui/*')
-    .pipe(gulp.dest('build/motion-ui'))
-  ;
-});
-
 gulp.task('build-css', function() {
 	gulp.src('scss/app.scss')
 	.pipe(sass({ includePaths : ['bower_components/foundation/scss'], outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(gulp.dest('css'));
 });
 
-gulp.task('build', function(cb) {
-  sequence('clean', ['copy-motion-ui'], cb);
-});
-
 gulp.task('default', function() {
-	gulp.start('build');
 	gulp.watch('scss/*.scss', ['build-css']);
 })
